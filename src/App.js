0bf99f4ecb248c941c7606075a426a3b386bfac3
Pedro Zamecki <51971866+PedroZamecki@users.noreply.github.com>
2024-12-02 15:17:09 -0300

    Refatora componente Team para utilizar dados dinâmicos da API e remove imagens estáticas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import { Outlet } from "react-router-dom";
-import ServerService from "./services/ServerService";
-
-const server = new ServerService()
-server.get('/api/configuracoes').then(
-  (response) => {
-    console.log(response)
-  }
-)
 
 function App() {
   return (
