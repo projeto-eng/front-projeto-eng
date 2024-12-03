1817ddf00f016d6a5f055e47e1e73c6ef2630ec4
Joao Pedro <joao.pedro.costa@usp.br>
2024-12-03 13:34:02 -0300

    confirmação na tela de cadastro

--- src/components/Cadastro/Cadastro.js
+++ src/components/Cadastro/Cadastro.js
@@ -4,20 +4,23 @@ import './Cadastro.css';
 import server from '../../services/ServerService';
 import { v4 as uuidv4 } from 'uuid';
 
+const defaultForm = {
+  nome: '',
+  sobrenome: '',
+  email: '',
+  dataNascimento: '',
+  escola: '',
+  uf: '',
+  pass: '',
+  confirmarSenha: ''
+}
+
 function Cadastro() {
   const [isStudent, setIsStudent] = useState(false);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [formData, setFormData] = useState({
-    nome: '',
-    sobrenome: '',
-    email: '',
-    dataNascimento: '',
-    escola: '',
-    uf: '',
-    pass: '',
-    confirmarSenha: ''
-  });
+  const [formData, setFormData] = useState(defaultForm);
+  const [createOk, setCreate] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -52,6 +55,9 @@ function Cadastro() {
     try {
       const response = await server.post('/api/accounts/create-user', usuario);
       console.log(response);
+      setError(null);
+      setFormData(defaultForm);
+      setCreate(true);
       // Handle successful registration (e.g., redirect or show success message)
     } catch (error) {
       console.error(error);
@@ -60,36 +66,46 @@ function Cadastro() {
       setLoading(false);
     }
   };
+  
 
   return (
     <div className="cadastro-container">
       <h2>Cadastro</h2>
-      {error && <div className="error-message">{error}</div>}
-      <form className="cadastro-form" onSubmit={handleSubmit}>
-        <input type="text" name="nome" placeholder="Nome" required value={formData.nome} onChange={handleChange} />
-        <input type="text" name="sobrenome" placeholder="Sobrenome" required value={formData.sobrenome} onChange={handleChange} />
-        <input type="email" name="email" placeholder="Email" required value={formData.email} onChange={handleChange} />
-        <input type="date" name="dataNascimento" placeholder="Data de Nascimento" required value={formData.dataNascimento} onChange={handleChange} />
-        <input type="text" name="uf" placeholder="UF" required value={formData.uf} onChange={handleChange} maxLength="2" />
-        <div className="checkbox-group">
-          <label>
-            <input
-              type="checkbox"
-              checked={isStudent}
-              onChange={() => setIsStudent(!isStudent)}
-            />
-            Sou aluno
-          </label>
+      {createOk? 
+      <div style={{alignItems: 'center'}}>
+        <div>Usuário criado com sucesso!</div>
+        <button className='ok-button' onClick={() => setCreate(false)}>OK! </button>
+      </div>
+      :
+      <div>
+        {error && <div className="error-message">{error}</div>}
+        <form className="cadastro-form" onSubmit={handleSubmit}>
+          <input type="text" name="nome" placeholder="Nome" required value={formData.nome} onChange={handleChange} />
+          <input type="text" name="sobrenome" placeholder="Sobrenome" required value={formData.sobrenome} onChange={handleChange} />
+          <input type="email" name="email" placeholder="Email" required value={formData.email} onChange={handleChange} />
+          <input type="date" name="dataNascimento" placeholder="Data de Nascimento" required value={formData.dataNascimento} onChange={handleChange} />
+          <input type="text" name="uf" placeholder="UF" required value={formData.uf} onChange={handleChange} maxLength="2" />
+          <div className="checkbox-group">
+            <label>
+              <input
+                type="checkbox"
+                checked={isStudent}
+                onChange={() => setIsStudent(!isStudent)}
+              />
+              Sou aluno
+            </label>
+          </div>
+          {isStudent && (
+            <input type="text" name="escola" placeholder="Nome da Escola" required value={formData.escola} onChange={handleChange} />
+          )}
+          <input type="password" name="pass" placeholder="Senha" required value={formData.pass} onChange={handleChange} autoComplete="new-password" />
+          <input type="password" name="confirmarSenha" placeholder="Confirmar Senha" required value={formData.confirmarSenha} onChange={handleChange} autoComplete="new-password" />
+          <button type="submit" disabled={loading}>
+            {loading ? 'Cadastrando...' : 'Cadastrar'}
+          </button>
+        </form>
         </div>
-        {isStudent && (
-          <input type="text" name="escola" placeholder="Nome da Escola" required value={formData.escola} onChange={handleChange} />
-        )}
-        <input type="password" name="pass" placeholder="Senha" required value={formData.pass} onChange={handleChange} autoComplete="new-password" />
-        <input type="password" name="confirmarSenha" placeholder="Confirmar Senha" required value={formData.confirmarSenha} onChange={handleChange} autoComplete="new-password" />
-        <button type="submit" disabled={loading}>
-          {loading ? 'Cadastrando...' : 'Cadastrar'}
-        </button>
-      </form>
+      }
     </div>
   );
 }
